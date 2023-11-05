53cf1477fa3f02cf5f78ee8335f7334a3c558cd7
Yoshidak-ta <bsk.gooserock@gmail.com>
2023-11-05 20:45:11 +0900

    本日日付記入

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
-const isDay = new Date("Seputember 22, 2023");
+const isDay = new Date("November 5, 2023");
 const day = isDay.getDate();
 const year = isDay.getFullYear();
-const month = isDay.getMonth();
+const month = isDay.getMonth() + 1;
 
 const theDay = () => {
   console.log(year + '年' + month + '月'+ day + '日');
